abf79d75e3517d65c6ca62727fc954c29558593f
S-a-b-b-a-S <yann.artho@gmail.com>
2024-12-08 23:55:46 +0100

    Update module.js
    Add message MP function

--- module.js
+++ module.js
@@ -10,3 +10,14 @@ Hooks.once("init", () => {
 Hooks.once("ready", () => {
     console.log("Fabulae GM Helper | Module ready to use.");
 });
+
+Hooks.on("chatMessage", (chatLog, messageText, chatData) => {
+    if (messageText.startsWith("/test")) {
+        console.log("Fabulae GM Helper | Test command detected.");
+        ChatMessage.create({
+            content: "Fabulae GM Helper is working!"
+        });
+        return false; // Empêche le message original de s'afficher
+    }
+});
+
